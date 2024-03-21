445c1536773d53996cb2a0fe5cab2a089259a804
samcasubuan <s.mcasubuan23@yahoo.com>
2024-03-21 14:56:37 +0800

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,52 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { RecipeList } from './RecipeList/RecipeList';
+import recipes from '../data/recipes.json';
+import { Component } from 'react';
+import { Statistics } from './Statistics/Statistics';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Section } from './Section/Section';
+
+export class App extends Component {
+  // initial state
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  handleClick = type => {
+    this.setState(prevState => ({
+      ...prevState,
+      [type]: prevState[type] + 1,
+    }));
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    const options = ['good', 'neutral', 'bad'];
+
+    return (
+      <>
+        <RecipeList recipes={recipes} />
+
+        <Section title="Please leave feedback">
+          {/* Create DOM to FeedbackOptions Component */}
+          <FeedbackOptions
+            options={options}
+            onLeaveFeedback={this.handleClick}
+          />
+        </Section>
+
+        <Section title="Statistics">
+          {/* Create DOM to Statistics Component */}
+          <Statistics good={good} neutral={neutral} bad={bad} total={total} />
+        </Section>
+      </>
+    );
+  }
+}
